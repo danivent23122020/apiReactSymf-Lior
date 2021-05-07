aed3c02c972a0efa5a9ce67018b370814c267e56
Danivent <dan.vent@laposte.net>
2021-05-07 22:41:24 +0200

    fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,42 +4,68 @@ namespace App\DataFixtures;
 
 use App\Entity\Customer;
 use App\Entity\Invoice;
+use App\Entity\User;
 use Faker\Factory;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * L'encodeur de mot de passe
+     *
+     * @var UserPasswordEncoderInterface
+     */
+    private $encoder;
+    
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+    
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('fr_FR');
 
-        $chrono = 1;
-
-        for($c =0; $c < 30; $c++){
-            $customer = new Customer();
-            $customer->setFirstName($faker->firstName)
-                    ->setLastName($faker->lastName)
-                    ->setCompany($faker->company)
-                    ->setEmail($faker->email);
+        
+        for($u =0; $u < 10; $u++){
+            $user = new User();
+            $chrono = 1;
+            $hash = $this->encoder->encodePassword($user, "password");
+            $user->setFirstName($faker->firstName())
+                ->setLastName($faker->lastName)
+                ->setEmail($faker->email)
+                ->setPassword($hash);
+            
+            $manager->persist($user);
+            
+            for($c =0; $c < mt_rand(5, 20); $c++){
+                $customer = new Customer();
+                $customer->setFirstName($faker->firstName())
+                        ->setLastName($faker->lastName)
+                        ->setCompany($faker->company)
+                        ->setEmail($faker->email)
+                        ->setUser($user);
+                        
+                $manager->persist($customer);
+                
+                for($i = 0; $i < mt_rand(3, 10); $i++){
+                    $invoice = new Invoice();
+                    $invoice->setAmount($faker->randomFloat(2, 250, 5000))
+                            ->setSentAt($faker->dateTimeBetween('- months'))
+                            ->setStatus($faker->randomElement([
+                                'SENT',
+                                'PAID',
+                                'CANCELLED'
+                            ]))
+                            ->setCustomer($customer)
+                            ->setChrono($chrono);
                     
-            $manager->persist($customer);
-
-            for($i = 0; $i < mt_rand(3,  10); $i++){
-                $invoice = new Invoice();
-                $invoice->setAmount($faker->randomFloat(2, 250, 5000))
-                        ->setSentAt($faker->dateTimeBetween('- months'))
-                        ->setStatus($faker->randomElement([
-                            'SENT',
-                            'PAID',
-                            'CANCELLED'
-                        ]))
-                        ->setCustomer($customer)
-                        ->setChrono($chrono);
-                
-                $chrono++;
-                
-                $manager->persist($invoice);
+                    $chrono++;
+                    
+                    $manager->persist($invoice);
+                }
             }
         }
         
