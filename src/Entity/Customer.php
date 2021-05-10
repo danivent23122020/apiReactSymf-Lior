9b0ab2f5ad32513b6d33720426928e0bdb7a2d65
Danivent <dan.vent@laposte.net>
2021-05-10 09:07:36 +0200

    Api

--- src/Entity/Customer.php
+++ src/Entity/Customer.php
@@ -7,6 +7,7 @@ use App\Repository\CustomerRepository;
 use ApiPlatform\Core\Annotation\ApiFilter;
 use Doctrine\Common\Collections\Collection;
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiSubresource;
 use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Serializer\Annotation\Groups;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
@@ -15,6 +16,9 @@ use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 /**
  * @ORM\Entity(repositoryClass=CustomerRepository::class)
  * @ApiResource(
+ *  subresourceOperations={
+ *      "invoices_get_subresource"={"path"="/clients/{id}/factures"}
+ *  },
  *  normalizationContext={
  *      "groups"={"customers_read"}
  *  }
@@ -59,6 +63,7 @@ class Customer
     /**
      * @ORM\OneToMany(targetEntity=Invoice::class, mappedBy="customer")
      * @Groups({"customers_read"})
+     * @ApiSubresource
      */
     private $invoices;
 
